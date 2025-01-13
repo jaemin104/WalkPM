dc6e2c6ce187527071a0f8b1ca8614bc827a6872
jaemin kim <kjm041107@hanyang.ac.kr>
2025-01-13 21:54:15 +0900

    제목 리스트 전달

--- lib/screens/routine_playlist_info.dart
+++ lib/screens/routine_playlist_info.dart
@@ -1,3 +1,67 @@
+// // routine_playlist_info.dart
+//
+// import 'package:flutter/material.dart';
+//
+// class RoutinePlaylistInfo extends StatelessWidget {
+//   final List<String> songTitles; // 전달받을 변수 선언
+//
+//   const RoutinePlaylistInfo({Key? key, required this.songTitles})
+//       : super(key: key); // 생성자 수정
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xFF9DA58E), // 배경 색상 설정
+//       appBar: AppBar(
+//         backgroundColor: Color(0xFF9DA58E),
+//         elevation: 0,
+//         leading: IconButton(
+//           icon: Icon(Icons.arrow_back, color: Colors.white),
+//           onPressed: () {
+//             Navigator.pop(context); // 뒤로 가기
+//           },
+//         ),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Text(
+//               '70~79BPM의 노래 4곡으로 14분 30초짜리\n출근길용 플레이리스트를 만들었어요.',
+//               style: TextStyle(
+//                 fontSize: 18,
+//                 color: Colors.white,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             Expanded(
+//               child: ListView.builder(
+//                 itemCount: songTitles.length,
+//                 itemBuilder: (context, index) {
+//                   return _buildSongTile(songTitles[index]);
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget _buildSongTile(String title) {
+//     return ListTile(
+//       title: Text(
+//         title,
+//         style: TextStyle(
+//           fontSize: 16,
+//           color: Colors.white,
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -5,12 +69,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RoutinePlaylistInfo(),
+      home: RoutinePlaylistInfo(songTitles: []), // 초기 빈 리스트
     );
   }
 }
 
 class RoutinePlaylistInfo extends StatelessWidget {
+  final List<String> songTitles; // 전달된 노래 제목 리스트
+
+  // 생성자에서 songTitles를 받음
+  RoutinePlaylistInfo({required this.songTitles});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,10 +90,7 @@ class RoutinePlaylistInfo extends StatelessWidget {
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => RoutinePlaylist()),
-            );
+            Navigator.pop(context); // 뒤로가기 버튼
           },
         ),
       ),
@@ -34,7 +100,7 @@ class RoutinePlaylistInfo extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              '70~79BPM의 노래 4곡으로 14분 30초짜리\n출근길용 플레이리스트를 만들었어요.',
+              '추천된 노래 제목들:',
               style: TextStyle(
                 fontSize: 18,
                 color: Colors.white,
@@ -42,139 +108,25 @@ class RoutinePlaylistInfo extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
-            Center(
-              child: Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(16.0),
-                    child: Image.asset(
-                      'assets/baekyerin.jpeg', // 이미지 경로 설정
-                      width: 150,
-                      height: 150,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF76876B),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                    child: Text(
-                      '재생',
+            // songTitles 리스트를 출력
+            Expanded(
+              child: ListView.builder(
+                itemCount: songTitles.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      songTitles[index],
                       style: TextStyle(
                         fontSize: 16,
                         color: Colors.white,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Expanded(
-              child: ListView(
-                children: [
-                  _buildSongTile(
-                    context,
-                    '산책',
-                    '백예린',
-                    '71BPM',
-                    '3분 34초',
-                    'assets/baekyerin.jpeg',
-                  ),
-                  _buildSongTile(
-                    context,
-                    'Madeleine Love',
-                    'CHEEZE(치즈)',
-                    '79BPM',
-                    '3분 34초',
-                    'assets/baekyerin.jpeg',
-                  ),
-                  _buildSongTile(
-                    context,
-                    'Home',
-                    '박효신',
-                    '75BPM',
-                    '3분 34초',
-                    'assets/baekyerin.jpeg',
-                  ),
-                  _buildSongTile(
-                    context,
-                    'Antifreeze',
-                    '백예린',
-                    '76BPM',
-                    '3분 34초',
-                    'assets/baekyerin.jpeg',
-                  ),
-                ],
+                  );
+                },
               ),
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget _buildSongTile(BuildContext context, String title, String artist,
-      String bpm, String duration, String imagePath) {
-    return ListTile(
-      leading: ClipRRect(
-        borderRadius: BorderRadius.circular(8.0),
-        child: Image.asset(
-          imagePath,
-          width: 50,
-          height: 50,
-          fit: BoxFit.cover,
-        ),
-      ),
-      title: Text(
-        title,
-        style: TextStyle(
-          fontSize: 16,
-          color: Colors.white,
-        ),
-      ),
-      subtitle: Text(
-        artist,
-        style: TextStyle(
-          fontSize: 14,
-          color: Colors.white70,
-        ),
-      ),
-      trailing: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            bpm,
-            style: TextStyle(
-              fontSize: 14,
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            duration,
-            style: TextStyle(
-              fontSize: 14,
-              color: Colors.white70,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class RoutinePlaylist extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Routine Playlist Page'),
       ),
     );
   }
--- lib/screens/walk_goal_result.dart
+++ lib/screens/walk_goal_result.dart
@@ -1,69 +1,158 @@
+// // walk_goal_result.dart
+//
+// import 'package:flutter/material.dart';
+// import 'package:spotify/screens/routine_playlist_info.dart'; // RoutinePlaylistInfo import 추가
+// import 'package:spotify/services/json_service.dart'; // JsonService 경로 수정
+//
+// class WalkGoalResultScreen extends StatefulWidget {
+//   final double bpm;
+//
+//   const WalkGoalResultScreen({Key? key, required this.bpm}) : super(key: key);
+//
+//   @override
+//   _WalkGoalResultScreenState createState() => _WalkGoalResultScreenState();
+// }
+//
+// class _WalkGoalResultScreenState extends State<WalkGoalResultScreen> {
+//   late Future<List<Map<String, dynamic>>> _songs;
+//   late List<String> songTitles = []; // 제목을 저장할 리스트 추가
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     // BPM 범위에 맞는 노래를 가져오는 메서드 사용
+//     _songs = JsonService()
+//         .getSongsByBPMRange(widget.bpm); // 수정: getSongsByBPMRange 메서드로 수정
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color(0xFFBDC9A3),
+//       appBar: AppBar(
+//         title: const Text("추천 노래 리스트"),
+//         backgroundColor: const Color(0xFF5C6E4F),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(20.0),
+//         child: Column(
+//           children: [
+//             Text(
+//               "추천 BPM: ${widget.bpm.toStringAsFixed(0)} BPM",
+//               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//             ),
+//             const SizedBox(height: 20),
+//             // BPM에 맞는 노래 리스트
+//             FutureBuilder<List<Map<String, dynamic>>>(
+//               future: _songs,
+//               builder: (context, snapshot) {
+//                 if (snapshot.connectionState == ConnectionState.waiting) {
+//                   return const CircularProgressIndicator();
+//                 } else if (snapshot.hasError) {
+//                   return Text('Error: ${snapshot.error}');
+//                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+//                   return const Text("추천된 노래가 없습니다.");
+//                 } else {
+//                   final songs = snapshot.data!;
+//                   // 노래 제목을 List<String>으로 변환
+//                   songTitles = List<String>.from(
+//                       songs.map((song) => song['title'] ?? 'Unknown Song'));
+//                   return Expanded(
+//                     child: ListView.builder(
+//                       itemCount: songs.length,
+//                       itemBuilder: (context, index) {
+//                         final song = songs[index];
+//                         return ListTile(
+//                           title: Text(song['title'] ?? 'Unknown Song'),
+//                           subtitle:
+//                               Text('BPM: ${song['Tempo'] ?? 'Unknown BPM'}'),
+//                           onTap: () {
+//                             // 노래 제목 리스트를 RoutinePlaylistInfo로 전달
+//                             Navigator.push(
+//                               context,
+//                               MaterialPageRoute(
+//                                 builder: (context) => RoutinePlaylistInfo(
+//                                   songTitles: songTitles,
+//                                 ),
+//                               ),
+//                             );
+//                           },
+//                         );
+//                       },
+//                     ),
+//                   );
+//                 }
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
-import 'package:spotify/services/json_service.dart'; // JsonService 경로 수정
+import 'routine_playlist_info.dart'; // RoutinePlaylistInfo 페이지 import
+import 'package:spotify/services/json_service.dart'; // JsonService 클래스를 import (경로를 맞게 수정)
 
 class WalkGoalResultScreen extends StatefulWidget {
   final double bpm;
 
-  const WalkGoalResultScreen({Key? key, required this.bpm}) : super(key: key);
+  WalkGoalResultScreen({required this.bpm});
 
   @override
   _WalkGoalResultScreenState createState() => _WalkGoalResultScreenState();
 }
 
 class _WalkGoalResultScreenState extends State<WalkGoalResultScreen> {
-  late Future<List<Map<String, dynamic>>> _songs;
+  late Future<List<String>> songTitles;
 
   @override
   void initState() {
     super.initState();
-    // BPM에 맞는 노래를 가져옴
-    _songs = JsonService().getSongsByBPMRange(widget.bpm); // bpm 값을 그대로 넘김
+    // getSongsByBPMRange()에서 제목만 추출하여 songTitles에 저장
+    songTitles = _getSongTitlesByBPM(widget.bpm);
+  }
+
+  // BPM 범위에 맞는 제목만 추출하는 메서드
+  Future<List<String>> _getSongTitlesByBPM(double bpm) async {
+    // songs를 BPM 범위에 맞는 곡들로 가져오기
+    final songs = await JsonService().getSongsByBPMRange(bpm);
+
+    // 제목을 List<String>으로 변환
+    return songs
+        .map<String>((song) => song['title'] ?? 'Unknown Song')
+        .toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFBDC9A3),
       appBar: AppBar(
-        title: const Text("추천 노래 리스트"),
-        backgroundColor: const Color(0xFF5C6E4F),
+        title: Text('Walk Goal Result'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             Text(
-              "추천 BPM: ${widget.bpm.toStringAsFixed(0)} BPM",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              'You have selected BPM: ${widget.bpm}',
+              style: TextStyle(fontSize: 18),
             ),
-            const SizedBox(height: 20),
-            // BPM에 맞는 노래 리스트
-            FutureBuilder<List<Map<String, dynamic>>>(
-              future: _songs,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Text("추천된 노래가 없습니다.");
-                } else {
-                  final songs = snapshot.data!;
-                  return Expanded(
-                    child: ListView.builder(
-                      itemCount: songs.length,
-                      itemBuilder: (context, index) {
-                        final song = songs[index];
-                        return ListTile(
-                          title: Text(song['title'] ?? 'Unknown Song'),
-                          subtitle:
-                              Text('BPM: ${song['Tempo'] ?? 'Unknown BPM'}'),
-                        );
-                      },
-                    ),
-                  );
-                }
+            // 버튼을 추가하여 RoutinePlaylistInfo 페이지로 이동
+            ElevatedButton(
+              onPressed: () async {
+                // getSongsByBPMRange 메서드를 통해 songTitles 리스트를 가져옴
+                List<String> titles = await songTitles;
+                // 버튼 클릭 시 RoutinePlaylistInfo로 이동하고 songTitles 전달
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        RoutinePlaylistInfo(songTitles: titles),
+                  ),
+                );
               },
+              child: Text('Go to Playlist Info'),
             ),
           ],
         ),
